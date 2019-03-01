ff6b96f50d5b144cb79593fbf8f2ad631e6f60eb
Yashwanth-thota <17yashwanth29@gmail.com>
2019-03-01 16:37:14 +0530

    Problem#2

---
+++ EnormousInputTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class EnormousInputTest
+{
+    public static void Main()
+    {
+        string[] inputs = Console.ReadLine().Split(' ');
+        int testCases=int.Parse(inputs[0]),divisor=int.Parse(inputs[1]);
+        int numberOfMulitiples = 0;
+        while (testCases > 0)
+        {
+            int temp = int.Parse(Console.ReadLine());
+            if (temp % divisor == 0) numberOfMulitiples++;
+            testCases--;
+        }
+        Console.WriteLine(numberOfMulitiples);
+        Console.ReadKey();
+    }
+}
