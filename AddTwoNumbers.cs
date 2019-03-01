f0cd4ec733133c189e0bc2836ec6bc5a2506338f
Yashwanth-thota <17yashwanth29@gmail.com>
2019-03-01 16:43:57 +0530

    Problem#3

---
+++ AddTwoNumbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class AddTwoNumbers
+{
+    public static void Main()
+    {
+        int testCases = int.Parse(Console.ReadLine());
+        string output = "";
+        while (testCases > 0)
+        {
+            string[] inputNumbers = Console.ReadLine().Split(' ');
+            output += int.Parse(inputNumbers[0])+int.Parse(inputNumbers[1])+"\n";
+            testCases--;
+        }
+        Console.WriteLine(output);
+        Console.ReadKey();
+    }
+}
